496407b00596afe0a2c7bff3d8144686da003f06
Evan Verma <edverma@icloud.com>
2025-06-12 23:26:18 -0700

    fix gss alias

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
     entry_points={
         "console_scripts": [
             "git-smart-squash=git_smart_squash.cli:main",
+            "gss=git_smart_squash.cli:main",
         ],
     },
     cmdclass={
